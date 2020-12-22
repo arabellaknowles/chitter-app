22e76f938f8750757dc37fd21132c68d082b6145
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-22 15:04:44 +0000

    Refactored posting a peep feature tests

--- spec/features/posting_a_peep_spec.rb
+++ spec/features/posting_a_peep_spec.rb
@@ -1,12 +1,4 @@
 feature 'user visits homepage' do
-  scenario 'user sees homepage with Chitter header' do
-    visit('/')
-    expect(page).to have_content('Welcome to Chitter')
-  end
-  scenario 'user can see textbox for peep on homepage' do
-    visit('/')
-    expect(page).to have_content('Make a peep')
-  end
   scenario 'user creates a peep and is redirected to homepage' do
     visit('/')
     fill_in 'peep_text', with: 'Day 44 of lockdown, still no loo roll'
