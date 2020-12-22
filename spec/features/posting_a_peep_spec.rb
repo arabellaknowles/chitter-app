cb6fa4c95faf172aa300e9b2f20f592c9487824a
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-22 15:07:57 +0000

    Minor edits to tests to improve readability

--- spec/features/posting_a_peep_spec.rb
+++ spec/features/posting_a_peep_spec.rb
@@ -1,8 +1,9 @@
-feature 'user visits homepage' do
-  scenario 'user creates a peep and is redirected to homepage' do
+feature 'posting a peep' do
+  scenario 'user creates a peep and is redirected to homepage with flash message' do
     visit('/')
     fill_in 'peep_text', with: 'Day 44 of lockdown, still no loo roll'
     click_on 'Post'
+    expect(page).to have_content('Welcome to Chitter')
     expect(page).to have_content('Successfully posted')
   end
 end
--- spec/units/peep_spec.rb
+++ spec/units/peep_spec.rb
@@ -1,12 +1,14 @@
-describe '.create' do
-  it 'creates a new peep' do
-    peep = Peep.create(peep: 'Hello, cruel world')
-    connection = PG.connect(dbname: 'chitter_app_manager_test')
-    result = connection.query("SELECT * FROM peeps WHERE id = #{peep.id};")
-    persisted_data = result.first
+describe Peep do
+  describe '.create' do
+    it 'creates a new peep' do
+      peep = Peep.create(peep: 'Hello, cruel world')
+      connection = PG.connect(dbname: 'chitter_app_manager_test')
+      result = connection.query("SELECT * FROM peeps WHERE id = #{peep.id};")
+      persisted_data = result.first
 
-    expect(peep).to be_a Peep
-    expect(peep.id).to eq persisted_data['id']
-    expect(peep.peep).to eq 'Hello, cruel world'
+      expect(peep).to be_a Peep
+      expect(peep.id).to eq persisted_data['id']
+      expect(peep.peep).to eq 'Hello, cruel world'
+    end
   end
 end
