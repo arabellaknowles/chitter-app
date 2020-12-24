50b532c29402845811916236060ebe8ca03d340a
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-24 11:32:53 +0000

    Feature: Make a peep form is not visible unless user is signed up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,8 @@ class Chitter < Sinatra::Base
   end
 
   post '/registrations' do
-    User.create(username: params[:username], email: params[:email], full_name: params[:full_name], password: params[:password])
+    current_user = User.create(username: params[:username], email: params[:email], full_name: params[:full_name], password: params[:password])
+    session[:current_user_id] = current_user.id
     flash[:notice] = 'Successfully signed up'
     redirect('/')
   end
--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -5,6 +5,11 @@ feature 'viewing peeps' do
     expect(page).to have_content('Pretty nice weather IMO')
   end
 
+  scenario 'user cannot see peep creation form if not signed up' do
+    visit('/')
+    expect(page).to_not have_content('Make a peep:')
+  end
+
   scenario 'user can view what time each peep was made' do
     @time_now = Time.now.strftime("%k:%M")
     allow(Peep).to receive(:current_time).and_return(@time_now)    
