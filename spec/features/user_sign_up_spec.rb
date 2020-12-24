187372dfccbbfd468e0993cbea84c87760ff12d1
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-24 11:45:53 +0000

    Deleted unneeded test from user_sign_up_spec

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -1,10 +1,4 @@
 feature 'user can sign up' do
-  scenario 'user visits home page clicks button for sign up and is redirected to sign up form' do
-    visit('/')
-    click_on('Sign up')
-    expect(page).to have_content('Username:')
-    expect(page).to have_content('Email:')
-  end
   scenario 'user fills in registration form and registers' do
     visit('/')
     click_on('Sign up')
