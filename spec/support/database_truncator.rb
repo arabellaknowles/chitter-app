c08214d44150707876716b5a3f6b9a10bb7ab6bf
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-24 15:00:24 +0000

    Added chitter users database to the truncator method

--- spec/support/database_truncator.rb
+++ spec/support/database_truncator.rb
@@ -2,4 +2,6 @@ def truncate_test_database
   p "Setting up test database..."
   connection = PG.connect(dbname: 'chitter_app_manager_test')
   connection.exec("TRUNCATE peeps;")
+  connection = PG.connect(dbname: 'chitter_users_test')
+  connection.exec("TRUNCATE users;")
 end
