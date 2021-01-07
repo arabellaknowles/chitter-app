5c095d13e22df67c5794bc76d60d2456f3e88221
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2021-01-07 13:18:28 +0000

    Refactor: Created test helper: post_peep method

--- spec/features/posting_a_peep_spec.rb
+++ spec/features/posting_a_peep_spec.rb
@@ -2,13 +2,13 @@ feature 'posting a peep' do
   scenario 'user creates a peep and is redirected to homepage with flash message' do
     visit('/')
     click_on('Sign up')
-    fill_in 'username', with: 'javascriptFanGirl'
-    fill_in 'email', with: 'JSLover@gmail.com'
-    fill_in 'full_name', with: 'Jane Doe'
-    fill_in 'password', with: 'Dogs 4 life'
-    click_on 'Register'
-    fill_in 'peep_text', with: 'Day 44 of lockdown, still no loo roll'
-    click_on 'Peep'
+    user_sign_up(
+      username: 'javascriptFanGirl', 
+      email: 'JSLover@gmail.com', 
+      full_name: 'Jane Doe', 
+      password: 'Dogs 4 life'
+    )
+    post_peep
     expect(page).to have_content('Welcome to Chitter')
     expect(page).to have_content('Successfully posted')
   end
--- spec/support/test_helpers.rb
+++ spec/support/test_helpers.rb
@@ -11,3 +11,8 @@ def user_sign_up(username:, email:, full_name:, password:)
   fill_in 'password', with: password
   click_on 'Register'
 end
+
+def post_peep
+  fill_in 'peep_text', with: 'Day 44 of lockdown, still no loo roll'
+  click_on 'Peep' 
+end
