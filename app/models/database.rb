bfc92d62c8d6daa8a05993c35258bd8bd54465b5
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2021-01-07 14:41:12 +0000

    Database class .query method created

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,5 +1,9 @@
 class Database
-  def self.connect(dbname:)
-    PG.connect(dbname: dbname)
+  def self.connect(dbname)
+    @connection = PG.connect(dbname: dbname)
+  end
+
+  def self.query(query)
+    @connection.exec(query)
   end
 end
--- spec/units/database_spec.rb
+++ spec/units/database_spec.rb
@@ -1,9 +1,18 @@
 describe Database do
   describe '.connect' do
     it 'connects to the database specified in the argument' do
-      expect(PG).to receive(:connect).with(dbname: 'chitter_manager_test')
+      expect(PG).to receive(:connect).with(dbname: 'chitter_app_manager_test')
 
-      Database.connect(dbname: 'chitter_manager_test')
+      Database.connect('chitter_app_manager_test')
+    end
+  end
+
+  describe '.query' do
+    it 'queries database with specified psql string argument' do
+      connection = Database.connect('chitter_app_manager_test')
+      expect(connection).to receive(:exec).with("SELECT * FROM users WHERE id='1'")
+
+      Database.query("SELECT * FROM users WHERE id='1'")
     end
   end
 end
